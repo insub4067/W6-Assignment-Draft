cece28ec290484564476242a0fecc7620c891fb4
coffeed-cat <gwcattower@gmail.com>
2021-07-09 21:28:20 +0900

    회원가입 성공

--- middlewares/registerValidater.js
+++ middlewares/registerValidater.js
@@ -2,65 +2,53 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const Joi = require("joi");
 
-
 const registerValidation = Joi.object({
-    email: Joi.string().email().required(),
-    nickname: Joi.string().alphanum().min(3).trim().required(), //알파벳+숫자, 최소3자이상, 공백제거하고 받음
-    password: Joi.string().min(4).trim().required(), //최소 4자이상, 공백제거하고 받음
-    confirmPassword: Joi.ref("password"),}).with("password", "confirmPassword");
+  email: Joi.string().email().required(),
+  nickname: Joi.string().alphanum().min(3).trim().required(), //알파벳+숫자, 최소3자이상, 공백제거하고 받음
+  password: Joi.string().min(4).trim().required(), //최소 4자이상, 공백제거하고 받음
+  confirmPassword: Joi.ref("password"),
+}).with("password", "confirmPassword");
 
+module.exports = async (req, res, next) => {
+  const { nickname, email, password, confirmPassword } = req.body;
+  try {
+    //회원가입 벨리데이션
+    // const { nickname, email, password, confirmPassword } = await registerValidation.validateAsync(req.body);
 
-module.exports = async(req, res, next) => {
+    if (email === password) {
+      res.status(400).send({
+        errorMessage: "이메일과 비밀번호는 동일하게 설정할 수 없습니다",
+      });
+    }
 
-    try {
-        
-        
-        //회원가입 벨리데이션 
-        // const { nickname, email, password, confirmPassword } = await registerValidation.validateAsync(req.body);
+    // 닉네임 중복검사
+    const existNick = await User.findOne({
+      $or: [{ email }, { nickname }],
+    });
 
-        if (email === password) {
-            res.status(400).send({
-                errorMessage: '이메일과 비밀번호는 동일하게 설정할 수 없습니다',
-            });
-        }     
-        
-        // 닉네임 중복검사 
-        const existNick = await User.findOne({ 
-            $or: [{email} , {nickname}],
-        });
-        
-        if (existNick) {
-            res.status(400).send({
-                errorMessage: '닉네임이 중복되었습니다',
-            });
-            return;
-        }
+    if (existNick) {
+      res.status(400).send({
+        errorMessage: "닉네임이 중복되었습니다",
+      });
+      return;
+    }
 
-        // 이메일 중복검사
-        const existemail = await User.findOne({ 
-            $or: [{email} , {nickname}],
-        });
-        
-        if (existemail) {
-            res.status(400).send({
-                errorMessage: '이메일이 중복되었습니다',
-            });
-            return;
-        }
-        
-        next();
+    // 이메일 중복검사
+    const existemail = await User.findOne({
+      $or: [{ email }, { nickname }],
+    });
 
-        } catch(err) {
-            res.status(401).send({
-                errorMessage: '요청한 형식이 올바르지 않습니다' 
-                });   
-                return;
-        }
-
-
-
+    if (existemail) {
+      res.status(400).send({
+        errorMessage: "이메일이 중복되었습니다",
+      });
+      return;
+    }
+    next();
+  } catch (err) {
+    res.status(401).send({
+      errorMessage: "요청한 형식이 올바르지 않습니다",
+    });
+    return;
+  }
 };
-
-
-
-
