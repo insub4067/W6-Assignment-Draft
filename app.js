13ac942e9b98c3b307412ab4f97e079c07d8e891
coffeed-cat <gwcattower@gmail.com>
2021-07-10 15:43:44 +0900

    소켓 추가

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const connect = require("./config");
 const mainRouter = require("./routers/index");
+const Http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
-const router = express.Router();
+const http = Http.createServer(app);
+const io = socketIo(http);
+
+io.on("connect", (socket) => {
+  // 좋아요 관련 이벤트
+  socket.on("HEART_UPDATED_FROM_FRONT", (data) => {
+    socket.emit("HEART_UPDATED_FROM_BACK", data);
+  });
+
+  // 상세 페이지 관련 이벤트
+  socket.on("WATCHING_DETAIL_FROM_FRONT", (productId) => {
+    socket.join(productId);
+
+    io.to(productId).emit("WATCHING_DETAIL_FROM_BACK", productId);
+
+    socket.on("COMMENT_CHANGED_FROM_FRONT", () => {});
+  });
+});
 
 app.use(
   "/",
@@ -12,9 +31,6 @@ app.use(
   mainRouter
 );
 
-app.use("/api", express.urlencoded({ extended: false }), router);
-app.use(express.json());
-
-app.listen(8080, () => {
+http.listen(8080, () => {
   console.log("서버가 요청을 받을 준비가 됐어요");
 });
