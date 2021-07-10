8e42c229561323affc3476b8bcd189077909c2b5
최완호 <dhksgh9506@icloud.com>
2021-07-10 17:43:19 +0900

    encryted password

--- routers/user.js
+++ routers/user.js
@@ -5,10 +5,10 @@ const registerValidator = require("../middlewares/registerValidater")
 const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const secretKey = require("../config/secretKey");
-
-
+const multer = require("multer")
 const User = require("../models/user");
 const { route } = require("./heart");
+const crypto = require('crypto');
 
 
 //회원가입
@@ -17,7 +17,9 @@ router.post('/register', registerValidator , async (req, res) => {
 
     try {
         const { email, nickname, password } = req.body;
+        const encryptedPassword = crypto.createHash('sha512').update(password).digest('base64'); //암호화 
         const user = new User({ email, nickname, password });
+        user.password = encryptedPassword
         await user.save(); //
         res.status(201).send({ result: '개꿀' });
 
@@ -40,22 +42,22 @@ const loginValidater = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(4).trim().required(),
 });
-
 //로그인 
 router.post('/login', async (req, res) => {
 
     try {
-        const { email, password } = await loginValidater.validateAsync(
+        let { email, password } = await loginValidater.validateAsync(
             req.body
         );
-
-        const user = await User.findOne({ email, password }).exec();
+        const encryptedPassword = crypto.createHash('sha512').update(password).digest('base64'); //암호화 
+        const user = await User.findOne({ $and : 
+            [{ email: email}, {password : encryptedPassword }] })
+        
 
         if (!user) {
             res.status(401).send({ errorMessage: '로그인에 실패했습니다. ' });
             return;
         }
-
         const token = jwt.sign( //토큰 발급
             { email: user.email, nickname: user.nickname, userId: user.userId },
             secretKey
